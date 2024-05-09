2971e6ed4b5ea3e4e3a12b93fa8caacf7584a829
estebanquito123 <estebantoro832@gmail.com>
2024-05-09 01:51:36 -0400

    api

---
+++ javascript/scriptRazas.js
@@ -0,0 +1,40 @@
+function getRazas(done) {
+    const results = fetch("https://api.thedogapi.com/v1/breeds")
+
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        })
+}
+
+getRazas(data => {
+    data.forEach(dogBreed => {
+        // Hacer una solicitud para obtener la URL de la imagen usando reference_image_id
+        fetch(`https://api.thedogapi.com/v1/images/${dogBreed.reference_image_id}`)
+            .then(response => response.json())
+            .then(imageData => {
+                const imageUrl = imageData.url;
+                const article = document.createRange().createContextualFragment(/*html */`
+                    <article>
+                        <div class="image-container">
+                            <img src="${imageUrl}" alt="raza">
+                        </div>
+                        <h2>${dogBreed.name}</h2>
+                        <span> ${dogBreed.temperament} </span>
+                        <p>${dogBreed.bred_for}</p>
+                        
+                    </article>
+                `);
+                const main = document.querySelector("main")
+                main.append(article)
+            })
+            .catch(error => {
+                console.error('Error al obtener la URL de la imagen:', error.message);
+            });
+    });
+});
+
+
+
+
