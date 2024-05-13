9dbc1d03b183ca92b3b41de1e968ffac3463eba7
Patricio Finschi <patofinschi@gmail.com>
2024-05-12 23:29:54 -0400

    Index
    Se cambia icono para ingresar a la pagina de registro y se agrega la pagina de registro.

---
+++ JS/JScript.js
@@ -0,0 +1,28 @@
+const formulario = document.getElementById('formulario');
+
+const inputs = document.querySelectorAll('#formulario input');
+
+const expresiones = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    password: /^.{4,12}$/
+}
+
+const campos = {
+    nombre: false,
+    apellido: false,
+    correo: false,
+    password: false
+}
+
+const validarFormulario = (e) => {
+    switch (e.target.name){
+        case "nombre":
+            validarCampo(expresiones.nombre, e.target, 'nombre');
+        break;
+        case "apellido":
+            validarCampo(expresiones.nombre, e.target, 'apellido');
+        break;
+    }
+}
